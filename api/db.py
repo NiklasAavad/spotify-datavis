00ec3024b6c99700bf21a0d7272204c4a3995b6d
Niklas <niklasaavad@gmail.com>
2023-10-04 22:55:55 +0200

    chore: stacking the db part 1

--- api/db.py
+++ api/db.py
@@ -5,25 +5,50 @@ track_artist_dict = {}
 artist_genre_dict = {}
 all_artist_ids = []
 
+# Paths for db
+track_artist_dict_path = 'track_artist_dict.json'
+artist_genre_dict_path = 'artist_genre_dict.json'
+all_artist_ids_path = 'all_artist_ids.json'
+index_of_how_far_we_made_it_path = 'index_of_how_far_we_made_it.json'
+
 def load_db():
     global track_artist_dict
     global artist_genre_dict
     global all_artist_ids
 
-    with open('track_artist_dict.json') as json_file:
+    with open(track_artist_dict_path) as json_file:
         track_artist_dict = json.load(json_file)
 
-    with open('artist_genre_dict.json') as json_file:
+    with open(artist_genre_dict_path) as json_file:
         artist_genre_dict = json.load(json_file)
 
-    with open('all_artist_ids.json') as json_file:
+    with open(all_artist_ids_path) as json_file:
         all_artist_ids = json.load(json_file)
+
+    with open(index_of_how_far_we_made_it_path) as json_file:
+        return json.load(json_file)
+
+def save_db(index_of_how_far_we_made_it):
+    with open(index_of_how_far_we_made_it_path, 'w') as outfile:   
+        json.dump(index_of_how_far_we_made_it, outfile)
+
+    with open(track_artist_dict_path, 'w') as outfile:
+        json.dump(track_artist_dict, outfile)
+
+    with open(artist_genre_dict_path, 'w') as outfile:
+        json.dump(artist_genre_dict, outfile)
+
+    with open(all_artist_ids_path, 'w') as outfile:
+        json.dump(all_artist_ids, outfile)
 
 def is_track_saved(track_id):
     return track_id in track_artist_dict
 
 def add_track_artists(track_id, artist_ids):
     track_artist_dict[track_id] = artist_ids
+
+def get_tracks_artist_db():
+    return track_artist_dict
 
 def is_artist_saved(artist_id):
     return artist_id in artist_genre_dict
@@ -34,13 +59,26 @@ def add_artist_genre(artist_id, genre):
 def write_artist_ids(artist_ids):
     global all_artist_ids
     all_artist_ids += artist_ids
-    # TODO write to file?
+
+def OVERwrite_artist_ids(artist_ids):
+    global all_artist_ids
+    all_artist_ids = artist_ids
+
+def get_artist_ids_from_db():
+    return all_artist_ids
+
+def load_how_far_we_made_it():
+    with open(index_of_how_far_we_made_it_path) as json_file:
+        return json.load(json_file)
+
 
 # TODO only for testing purposes
 def print_db():
-    print("PRINTING DB")
-    print(len(track_artist_dict))
-    print(len(artist_genre_dict))
-    print("PRINTING ALL ARTIST IDS")
-    print(len(all_artist_ids))
-    print("DONE PRINTING DB")
+    with open(index_of_how_far_we_made_it_path, 'w') as outfile:   
+        json.dump(-1, outfile)
+
+if __name__ == "__main__":
+    idx = load_db()
+    print("we made it this far:", idx)
+    print("and have this many items in our artist list:", len(all_artist_ids))
+    print("and this many items in our track_artist_dict:", len(track_artist_dict))
--- api/preprocessing.py
+++ api/preprocessing.py
@@ -1,21 +1,69 @@
+from pickle import load
 import pandas as pd
 from query import get_artist_ids, get_several_tracks
-from db import write_artist_ids
+from db import write_artist_ids, save_db, load_db, get_artist_ids_from_db, OVERwrite_artist_ids, is_track_saved, get_tracks_artist_db
+import json
 
 def write_to_db(file_path):
     chunk_size = 50
     chunks = pd.read_csv(file_path, chunksize=chunk_size)
 
-    all_artist_ids = []
+    unsaved_artists = []
 
-    for i, chunk in enumerate(chunks):
+    start = load_db() + 1
+
+    for i, chunk in enumerate(chunks, start=start):
         print('Processing chunk:', i+1)
         urls = [url.split('/')[-1] for url in chunk['url']]
         track_responses = get_several_tracks(urls)
         tracks = track_responses['tracks']
-        all_artist_ids += get_artist_ids(tracks)
-    
-    write_artist_ids(all_artist_ids)
+        unsaved_artists += get_artist_ids(tracks)
+        if i % 50 == 0:
+            print('Saving to db')
+            write_artist_ids(unsaved_artists)
+            save_db(i)
+            unsaved_artists = []
+   
+    # track -> artist dict will be written during the get_aritst_ids method
+    write_artist_ids(unsaved_artists)
+    save_db(-1) # mean we're done!!!
+
+def track_artist_for_tracks_not_in_db():
+    _ = load_db()
+
+    tracks_not_in_db = []
+    with open('../../charts_missing_from_db.json') as json_file:
+        tracks_not_in_db = json.load(json_file)
+        print("Loaded tracks not in db")
+
+    print("number of tracks not in db:", len(tracks_not_in_db))
+
+    batch_size = 50
+
+    unsaved_artists = []
+
+    for i in range(0, len(tracks_not_in_db), batch_size):
+        print(f'Processing #{i}')
+        batch = tracks_not_in_db[i : i+batch_size]
+        track_responses = get_several_tracks(batch)
+        tracks = track_responses['tracks']
+        unsaved_artists += get_artist_ids(tracks)
+        if i % 1000 == 0:
+            print('Saving to db')
+            write_artist_ids(unsaved_artists)
+            save_db(i)
+            unsaved_artists = []
+
+    write_artist_ids(unsaved_artists)
+    save_db(-1) # mean we're done!!!
+
+
+def remove_duplicates_from_artist_ids():
+    how_far_we_have_come = load_db()
+    artist_ids = get_artist_ids_from_db()
+    artist_ids = list(set(artist_ids))
+    OVERwrite_artist_ids(artist_ids)
+    save_db(how_far_we_have_come)
 
 def remove_duplicates(file_path, output_file_path):
     chunk_size = 1000000 
@@ -45,6 +93,32 @@ def remove_viral50(file_path, output_file_path):
     print('Saving top200 rows to file')
     filtered_rows.to_csv(output_file_path, index=False)  # index=False to exclude the index column
 
+def get_id_from_url(url):
+    return url.split('/')[-1]
+
+
+def remove_tracks_that_are_in_db(file_path, output_file_path):
+    _ = load_db()
+
+    chunk_size = 10000
+    chunks = pd.read_csv(file_path, chunksize=chunk_size)
+
+    tracks_db = get_tracks_artist_db()
+    keys = tracks_db.keys()
+
+    tracks_missing_in_db = []
+
+    for i, chunk in enumerate(chunks):
+        print('Processing chunk:', i)
+        for row in chunk.itertuples(index=False):
+            url = row.url
+            id = get_id_from_url(url)
+            if id not in keys:
+                tracks_missing_in_db.append(id)
+
+    with open(output_file_path, 'w') as outfile:
+        json.dump(tracks_missing_in_db, outfile)
+
 def check_number_of_rows_in_chart(file_path):
     chunk_size = 1000000 
     chunks = pd.read_csv(file_path, chunksize=chunk_size)
@@ -57,6 +131,30 @@ def check_number_of_rows_in_chart(file_path):
 
     print('Number of rows in chart:', rows)
 
+def check_if_all_tracks_is_in_db(file_path):
+    chunk_size = 50
+    chunks = pd.read_csv(file_path, chunksize=chunk_size)
+
+    last_chunk_to_check = load_db()
+
+    tracks_not_in_db = 0
+
+    for i, chunk in enumerate(chunks):
+        tracks_missing_from_current_chunk = 0
+        urls = [url.split('/')[-1] for url in chunk['url']]
+        for url in urls:
+            if not is_track_saved(url):
+                tracks_not_in_db += 1
+                tracks_missing_from_current_chunk += 1
+        print(f'Tracks missing from chunk {i}:', tracks_missing_from_current_chunk)
+
+    print("Number of tracks, NOT in db:", tracks_not_in_db)
+        
+
 if __name__ == "__main__":
-    file_path = '../../unique_charts.csv'
-    check_number_of_rows_in_chart(file_path)
+    """ file_path = '../../unique_charts.csv' """
+    """ write_to_db(file_path) """
+    """ check_if_all_tracks_is_in_db(file_path) """
+    """ output_path = "../../charts_missing_from_db.json" """
+    """ remove_tracks_that_are_in_db(file_path, output_path) """
+    track_artist_for_tracks_not_in_db()
