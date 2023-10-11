a7e56c725b2ce10346de21f2dab8a7e4b41b27de
Niklas <niklasaavad@gmail.com>
2023-10-11 21:13:45 +0200

    feat: country name as tooltip

--- vis/src/map.tsx
+++ vis/src/map.tsx
@@ -6,15 +6,43 @@ type MapProps = {
 }
 
 export const Map = ({ countries }: MapProps) => {
-	const width = 800;
-	const height = 800;
+	const width = 1200;
+	const height = 700;
 
 	const projection = d3
 		.geoMercator()
 		.scale(width / 2 / Math.PI - 40)
-		.center([10, 35])
+		.center([-40, 65])
 
 	const geoPathGenerator = d3.geoPath().projection(projection);
+
+	const mouseOver = (data: string) => {
+		d3.select('#tooltip')
+			.transition()
+			.duration(200)
+			.style('opacity', 0.9)
+			.text(data)
+	}
+
+	const mouseLeave = () => {
+		d3.select('#tooltip')
+			.transition()
+			.duration(200)
+			.style('opacity', 0)
+	}
+
+	const mouseMove = (event: React.MouseEvent<SVGPathElement, MouseEvent>) => {
+		d3.select('#tooltip')
+			.style('left', (event.pageX + 10) + 'px')
+			.style('top', (event.pageY - 10) + 'px')
+	}
+
+	// tooltip creation
+	d3.select('body')
+		.append('div')
+		.attr('id', 'tooltip')
+		.attr('style', 'position: absolute; opacity: 0;')
+
 
 	const allSvgPaths = countries.features
 		.filter(shape => shape.properties?.ADMIN !== 'Antarctica')
@@ -25,12 +53,15 @@ export const Map = ({ countries }: MapProps) => {
 				strokeWidth={0.5}
 				fill="grey"
 				fillOpacity={0.7}
+				onMouseOver={() => mouseOver(shape.properties?.ADMIN)}
+				onMouseLeave={mouseLeave}
+				onMouseMove={mouseMove}
 			/>
 		})
 
 	return (
 		<>
-			<svg width={width} height={height}>
+			<svg style={{ border: 'solid' }} width={width} height={height}>
 				{allSvgPaths}
 			</svg>
 		</>
