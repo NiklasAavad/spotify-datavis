f23e19d17c7b981ffa17e919f400a578b514e889
Niklas <niklasaavad@gmail.com>
2023-10-12 21:09:17 +0200

    chore: removed unnessecary dependencies for useEffect

--- vis/src/map.tsx
+++ vis/src/map.tsx
@@ -43,7 +43,7 @@ export const Map = () => {
 			.on('zoom', zoomed)
 
 		svg.call(zoom as any)
-	}, [geoPathGenerator, projection])
+	}, [])
 
 	const countrySvgPaths = countries.features
 		.map(countryFeature => <Country
