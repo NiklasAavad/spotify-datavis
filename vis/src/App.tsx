da078e3c14b329c56dcf49a865d45f0942760a37
Niklas <niklasaavad@gmail.com>
2023-10-12 09:53:32 +0200

    feat: added much smaller map

--- vis/src/App.tsx
+++ vis/src/App.tsx
@@ -1,10 +1,13 @@
-import { Map } from './map'
+/* import { Map } from './map' */
+/* import { countries } from './countries.ts' */
 import './App.css'
-import { countries } from './countries.ts'
+import { countries } from './countries2'
+import { Map } from './map2'
 
 function App() {
 	return (
 		<>
+			{/* <Map countries={countries} /> */}
 			<Map countries={countries} />
 		</>
 	)
