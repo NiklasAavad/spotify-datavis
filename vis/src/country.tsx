8db879d8034b8440f5837880ef748870e5a59751
Niklas <niklasaavad@gmail.com>
2023-10-12 21:08:20 +0200

    refactor: extracted country to own component

---
+++ vis/src/country.tsx
@@ -0,0 +1,63 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as d3 from "d3"
+import { Feature, Geometry } from "geojson"
+
+type CountryProps = {
+	countryFeature: Feature<Geometry, {
+		[name: string]: any
+	}>,
+	selectedCountries: Set<string>,
+	setSelectedCountries: (newSet: Set<string>) => void,
+	geoPathGenerator: d3.GeoPath<any, d3.GeoPermissibleObjects>
+}
+
+export const Country: React.FC<CountryProps> = (props) => {
+	const { countryFeature, selectedCountries, setSelectedCountries, geoPathGenerator } = props
+
+	const handleClick = (country: string) => {
+		const newSet = new Set(selectedCountries)
+		if (selectedCountries.has(country)) {
+			newSet.delete(country)
+		} else {
+			newSet.add(country)
+		}
+		setSelectedCountries(newSet)
+	}
+
+	const handleMouseOver = (country: string) => {
+		d3.select('#tooltip')
+			.style('opacity', 0.9)
+			.classed('unselectable', true)
+			.text(country)
+	}
+
+	const handleMouseLeave = () => {
+		d3.select('#tooltip')
+			.style('opacity', 0)
+	}
+
+	const mouseMove = (event: React.MouseEvent<SVGPathElement, MouseEvent>) => {
+		d3.select('#tooltip')
+			.style('left', (event.pageX + 10) + 'px')
+			.style('top', (event.pageY - 10) + 'px')
+	}
+
+	const getFill = (country: string) => {
+		if (selectedCountries.has(country)) {
+			return 'red'
+		} else {
+			return 'grey'
+		}
+	}
+
+	return <path
+		d={geoPathGenerator(countryFeature)}
+		stroke="lightgrey"
+		strokeWidth={0.5}
+		fill={getFill(countryFeature.properties?.name)}
+		fillOpacity={0.7}
+		onClick={() => handleClick(countryFeature.properties?.name)}
+		onMouseOver={() => handleMouseOver(countryFeature.properties?.name)}
+		onMouseLeave={handleMouseLeave}
+		onMouseMove={mouseMove} />
+}
--- vis/src/map.tsx
+++ vis/src/map.tsx
@@ -3,6 +3,7 @@ import * as d3 from 'd3';
 import { useEffect, useRef, useState } from 'react';
 import './map.css'
 import { countries } from './countries.ts'
+import { Country } from './country.tsx';
 
 export const Map = () => {
 	const width = 1200;
@@ -44,62 +45,19 @@ export const Map = () => {
 		svg.call(zoom as any)
 	}, [geoPathGenerator, projection])
 
-	const handleClick = (country: string) => {
-		const newSet = new Set(selectedCountries)
-		if (selectedCountries.has(country)) {
-			newSet.delete(country)
-		} else {
-			newSet.add(country)
-		}
-		setSelectedCountries(newSet)
-	}
-
-	const handleMouseOver = (country: string) => {
-		d3.select('#tooltip')
-			.style('opacity', 0.9)
-			.classed('unselectable', true)
-			.text(country)
-	}
-
-	const handleMouseLeave = () => {
-		d3.select('#tooltip')
-			.style('opacity', 0)
-	}
-
-	const mouseMove = (event: React.MouseEvent<SVGPathElement, MouseEvent>) => {
-		d3.select('#tooltip')
-			.style('left', (event.pageX + 10) + 'px')
-			.style('top', (event.pageY - 10) + 'px')
-	}
-
-	const getFill = (country: string) => {
-		if (selectedCountries.has(country)) {
-			return 'red'
-		} else {
-			return 'grey'
-		}
-	}
-
-	const allSvgPaths = countries.features
-		.map(shape => {
-			return <path
-				d={geoPathGenerator(shape)}
-				stroke="lightgrey"
-				strokeWidth={0.5}
-				fill={getFill(shape.properties?.name)}
-				fillOpacity={0.7}
-				onClick={() => handleClick(shape.properties?.name)}
-				onMouseOver={() => handleMouseOver(shape.properties?.name)}
-				onMouseLeave={handleMouseLeave}
-				onMouseMove={mouseMove}
-			/>
-		})
+	const countrySvgPaths = countries.features
+		.map(countryFeature => <Country
+			countryFeature={countryFeature}
+			selectedCountries={selectedCountries}
+			setSelectedCountries={setSelectedCountries}
+			geoPathGenerator={geoPathGenerator} />
+		);
 
 	return (
 		<>
 			<div style={{ height: height, width: width, overflow: 'hidden', border: 'solid' }}>
 				<svg ref={svgRef} width={width} height={height}>
-					<g>{allSvgPaths}</g>
+					<g>{countrySvgPaths}</g>
 				</svg>
 			</div>
 		</>
