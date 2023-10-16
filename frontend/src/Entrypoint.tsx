7dd2e22ec407044054a42dfaa58f8d3341edefee
Niklas <niklasaavad@gmail.com>
2023-10-16 17:43:57 +0200

    feat: allow for frontend development without running backend

--- frontend/src/Entrypoint.tsx
+++ frontend/src/Entrypoint.tsx
@@ -16,17 +16,66 @@ const fetchDataFunction = async () => {
 	}
 };
 
+const useScores = () => {
+	const getScores = async () => {
+		try {
+			console.log("axios start baby")
+			const response = await axios.get('http://localhost:5000/test');
+			console.log("axios done baby")
+			console.log(response.data)
+			return response.data;
+		} catch (error) {
+			throw new Error('Error fetching data from the API');
+		}
+	};
+
+	return useQuery('scores', getScores);
+}
+
+const getData = () => {
+    if (!SHOULD_USE_BACKEND) {
+        return {
+          "USA": 90,
+          "Canada": 80,
+          "Mexico": 85,
+          "Spain": 80,
+          "Portugal": 90,
+          "France": 70,
+          "Italy": 75,
+          "Germany": 20,
+          "England": 75,
+          "Denmark": 90,
+          "Sweden": 15,
+          "Norway": 80,
+          "Finland": 90,
+          "Russia": 70,
+          "China": 10,
+          "Japan": 50,
+          "South Korea": 60,
+          "India": 30,
+          "Australia": 80,
+          "New Zealand": 90,
+        };
+    }
+
+    const { data, isLoading, isError, error } = useScores();
+
+    if (isError) {
+        console.log("error:", error);
+        throw new Exception("damn error");
+    }
+
+    if (!isLoading) {
+        return data;
+    }
+
+    console.log("Loading...");
+}
+
+const SHOULD_USE_BACKEND = false;
+
 export const Entrypoint = () => {
-	const { data, isLoading, isError, error } = useQuery('scores', fetchDataFunction);
-
-	if (isError) {
-		console.log("error:", error)
-		return <div>Error... </div>
-	}
-
-	if (isLoading) {
-		return <div>Loading...</div>
-	}
+    const data = getData();
 
 	const countryScores = new Map<string, number>(Object.entries(data));
 	console.log(countryScores)
