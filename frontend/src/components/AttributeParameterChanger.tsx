216b034e9f9973e338f96bf6be7bfeef3b67a42a
Niklas <niklasaavad@gmail.com>
2023-11-05 23:29:33 +0100

    fix: react warning regarding checked vs default checked attributes

--- frontend/src/components/AttributeParameterChanger.tsx
+++ frontend/src/components/AttributeParameterChanger.tsx
@@ -29,13 +29,13 @@ export const AttributeParameterChanger: React.FC<AttributeParameterChangerProps>
 		<>
 			<div>Attribute:</div>
 			<div onChange={onChangeAttribute}>
-				<input type="radio" value={Attribute.Danceability} checked={attribute == Attribute.Danceability} /> Danceability
-				<input type="radio" value={Attribute.Energy} checked={attribute == Attribute.Energy} /> Energy
-				<input type="radio" value={Attribute.Valence} checked={attribute == Attribute.Valence} /> Valence
-				<input type="radio" value={Attribute.Acousticness} checked={attribute == Attribute.Acousticness} /> Acousticness
-				<input type="radio" value={Attribute.Instrumentalness} checked={attribute == Attribute.Instrumentalness} /> Instrumentalness
-				<input type="radio" value={Attribute.Liveness} checked={attribute == Attribute.Liveness} /> Liveness
-				<input type="radio" value={Attribute.Speechiness} checked={attribute == Attribute.Speechiness} /> Speechiness
+				<input type="radio" value={Attribute.Danceability} defaultChecked={attribute == Attribute.Danceability} /> Danceability
+				<input type="radio" value={Attribute.Energy} defaultChecked={attribute == Attribute.Energy} /> Energy
+				<input type="radio" value={Attribute.Valence} defaultChecked={attribute == Attribute.Valence} /> Valence
+				<input type="radio" value={Attribute.Acousticness} defaultChecked={attribute == Attribute.Acousticness} /> Acousticness
+				<input type="radio" value={Attribute.Instrumentalness} defaultChecked={attribute == Attribute.Instrumentalness} /> Instrumentalness
+				<input type="radio" value={Attribute.Liveness} defaultChecked={attribute == Attribute.Liveness} /> Liveness
+				<input type="radio" value={Attribute.Speechiness} defaultChecked={attribute == Attribute.Speechiness} /> Speechiness
 			</div>
 		</>
 	)
